8eda8010b43dd1c173a33ee9cfb95aeafab79fc4
luisgarcian <69696717+luisgarcian@users.noreply.github.com>
2021-02-11 17:24:23 -0600

    Revert "VFPago Charts y Datatables  por cada serie de datos"
    This reverts commit 46dfdf33a97642d9c9e4faeba815eab5ec5d6adf.

--- js/datatables/vtasfpago.js
+++ js/datatables/vtasfpago.js
@@ -36,36 +36,32 @@ function DTable_vtasfpago(data) {
         fixedColumns:   true,
         language : {
           "emptyTable": "No se encuentran datos disponibles"
-        }
+        },
+        fnRowCallback: function( nRow, aData, iDisplayIndex ) {
+          /* All cells in first row will be bolded  */
+          if ( iDisplayIndex == 0 ) {
+              $('td', nRow).each(function(){
+                  $(this).addClass('bold');
+              });
+          }
+          return nRow;
+        },   
     }).draw();
   
   
- /*   $('table.display td').hover(function(){
-         $(this).css('background-color','#EC932F'); 
+   /* $('table.display tr').hover(function(){
+         $(this).css('background-color','#c9ced3'); // '#EC932F' naranja
     });
   
-    $('table.display td').mouseout(function(){
+    $('table.display tr').mouseout(function(){
          $(this).css('background-color','#f9f9f9'); 
     });   */
+   
+  }
   
-    $('#myTable tbody').on('click', 'tr', function () {
+
   
-      $(document).find('tr').removeClass("dtSelected");
-      $(miTabla.row(this).selector.rows).addClass("dtSelected");
-    
-      //Drill_Down Chart VFP por Sucursal
-      if ( sel_chart.nivel == 0 && sel_chart.seltipo == 0 ) { 
-        
-        var suc = this.children[0].innerText;
-        parms =  {
-          "fecini":  myPar1,
-          "fecfin":  myPar2,
-          "tipo"  :  suc,
-        }
-        if (suc != 'TOTAL') {  
-           VFP_Sucursal(suc, parms);
-        }
-      }
+  
   
     } );
   

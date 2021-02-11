21fb66e2c03075af4247f65efe15be6b09418373
luisgarcian <69696717+luisgarcian@users.noreply.github.com>
2021-02-11 17:37:48 -0600

    php faltantes

---
+++ js/charts/vtasfpago_dona.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_POST){
+   
+	if(! empty($_POST['fecini']) && ! empty($_POST['fecfin']) )
+	{
+	   // Se recuperan los parámetros
+	   $fecini=($_POST['fecini']);
+	   $fecfin=($_POST['fecfin']);
+	   $tipo=($_POST['tipo']);
+	
+   	   require '../datos/vtasfpago_dona.php';
+
+	   $Chart = new Conecta_Datos(); 
+	
+	   //Se trae los datos
+	   $consulta = $Chart -> Trae_Datos($fecini, $fecfin,  $tipo );
+
+	   //genera la salida de los datos en formato json
+       header('Content-type: application/json');
+	   echo json_encode($consulta);
+    }
+  
+}
+
+?>
